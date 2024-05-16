f94f1fcf5ab30aea617664167e0d6f3cd2ada30e
SergeyStarchenko <pdxserg@gmail.com>
2024-05-16 11:54:03 -0700

    js

--- src/10/10_01.test.tsx
+++ src/10/10_01.test.tsx
@@ -1,4 +1,4 @@
-import {moveUser, Userlaptop, UserType} from "./10_01";
+import {addNew, addNew2, moveUser, UserBookType, Userlaptop, UserType} from "./10_01";
 import {Simulate} from "react-dom/test-utils";
 import copy = Simulate.copy;
 
@@ -33,5 +33,41 @@ test('some test1',()=>{
 	expect(user.address).not.toBe(movedUser.address)
 	expect(user.laptop).toBe(movedUser.laptop)
 	expect(movedUser.address.title).toBe('Kiev')
+
+})
+test('update book',()=>{
+	let user: UserBookType={
+		name: 'bob',
+		hair:32,
+		address:{title:'Minsk'},
+		laptop:{title:'Asus'},
+		books:['css','js','react']
+	}
+	const copyUser=addNew(user, "ts")
+
+	expect(user).not.toBe(copyUser)
+	expect(user.address).not.toBe(copyUser.address)
+	expect(user.laptop).toBe(copyUser.laptop)
+	expect(user.books).not.toBe(copyUser.books)
+	expect(copyUser.books.length).toBe(4)
+	expect(copyUser.books[3]).toBe('ts')
+
+})
+test('update book2',()=>{
+	let user: UserBookType={
+		name: 'bob',
+		hair:32,
+		address:{title:'Minsk'},
+		laptop:{title:'Asus'},
+		books:['css','js','react']
+	}
+	const copyUser=addNew2(user, "ts", "js")
+
+	expect(user).not.toBe(copyUser)
+	expect(user.address).not.toBe(copyUser.address)
+	expect(user.laptop).toBe(copyUser.laptop)
+	expect(user.books).not.toBe(copyUser.books)
+	expect(copyUser.books.length).toBe(3)
+	expect(copyUser.books[1]).toBe('ts')
 
 })
--- src/10/10_01.tsx
+++ src/10/10_01.tsx
@@ -8,7 +8,18 @@ export type UserType={
 export type Userlaptop = UserType &{
 	laptop:{title:string}
 }
-
+export type UserBookType=UserType&Userlaptop& {
+	books:Array<string>
+}
 export function moveUser(user:Userlaptop, city:string){
 	return {...user,  address:{...user.address, title:city}}
+}
+
+export const addNew=(us:UserBookType,book:string, )=>{
+	return{...us, address:{...us.address},
+		books: [...us.books, book] }
+}
+export const addNew2=(us:UserBookType,book:string,change:string)=>{
+	return{...us, address:{...us.address},
+		books: us.books.map((b)=> b===change ? b=book : b) }
 }
