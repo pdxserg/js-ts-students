ac35ec534bb50631901951eec59bdb4ad6c684c5
SergeyStarchenko <pdxserg@gmail.com>
2024-06-23 20:56:06 -0700

    js

--- src/promises.js
+++ src/promises.js
@@ -1,19 +1,32 @@
-const promisefetch= ((url)=>{
-    return new Promise((res, rej)=>{
-        setTimeout(()=>{
-            // res ("resolve!!!")
-              rej ("reject!!! eror")
-        },200)
+// console.log("start")
+//
+// const promisefetch= ((url)=>{
+//     return new Promise((res, rej)=>{
+//         setTimeout(()=>{
+//             // res ("resolve!!!")
+//               rej ("reject!!! eror")
+//         },2000)
+//     })
+// })
+//
+// const promise = promisefetch("google.com")
+//      console.log(promise)
+//
+// promise.then ((data)=>{
+//     console.log(data)
+// })
+//
+//   promise.catch ((err)=>{
+//       console.log("Catch", err)
+//   })
+// promise.finally(()=>{
+//     console.log("Finally")
+// })
+
+
+const delay = (()=>{
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{},1000)
     })
 })
-
-const promise = promisefetch("google.com")
-     console.log(promise)
-
-promise.then ((data)=>{
-    console.log(data)
-})
-
-  promise.catch ((err)=>{
-      console.log("Catch", err)
-  })
+const promise = delay()
