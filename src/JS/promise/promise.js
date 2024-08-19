d9c93ac5b706d623fbe729d93bc484d01a34094e
SergeyStarchenko <pdxserg@gmail.com>
2024-08-18 18:17:39 -0700

    promise.html

--- src/JS/promise/promise.js
+++ src/JS/promise/promise.js
@@ -1,4 +1,4 @@
-Promise.resolve()
+// Promise.resolve()
 // .then(()=>{
 //     console.log(1)
 //     throw new Error()
@@ -7,12 +7,29 @@ Promise.resolve()
 //     .catch(()=>{console.log(3)})
 //     .finally(()=>{console.log(4)})
 //     .then(()=>{console.log(5)})
+//
+// Promise.resolve()
+//     .then(()=>{console.log(11)})
+//     .then(()=>{
+//         console.log(12)
+//         throw new Error()
+//     })
+//     .catch(()=>{console.log(13)})
+//     .then(()=>{console.log(14)})
 
-Promise.resolve()
-    .then(()=>{console.log(2)})
-    .then(()=>{
-        console.log(1)
-        throw new Error()
-    })
-    .catch(()=>{console.log(3)})
-    .then(()=>{console.log(5)})
+// const tasks = async ()=>{
+//     console.log(1)
+//     await new Promise((res,rej)=>{
+//         console.log(10)
+//         res()
+//     }).then(()=>{console.log(2)})
+//     setTimeout(()=>{
+//         Promise.resolve().then(()=>{console.log(3)})
+//         console.log(3.1)
+//         Promise.resolve().then(()=>{console.log(3.2)})
+//     },0)
+//     setTimeout(()=>{console.log(4)},0)
+//     Promise.resolve().then(()=>{console.log(5)})
+//     console.log(6)
+// }
+// tasks()
