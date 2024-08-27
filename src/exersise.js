5770afdbea93422b1ce3be071ea30ec39c6ec92f
SergeyStarchenko <pdxserg@gmail.com>
2024-08-26 21:03:24 -0700

    Js

--- src/exersise.js
+++ src/exersise.js
@@ -1,23 +1,34 @@
-let user ={
-    name: "Dimych",
-    age:13,
-    address:{
-        city:{
-            title: "Minsk"
-        }
+// let user ={
+//     name: "Dimych",
+//     age:13,
+//     address:{
+//         city:{
+//             title: "Minsk"
+//         }
+//     }
+// }
+// //console.log(user.address.city.title)
+// //console.log(user["address"]["city"]["title"])
+// let user1 ={
+//     '0': 'Dimych',
+//     '1': 'Valera'
+// }
+// // console.log(user1['0'])
+//  user1 ['table']= "brown"
+// // console.log(user1)
+//  console.log(user1["table"]) //brown
+//
+// console.log(Object.keys(user1))//[ '0', '1', 'table' ]
+//
+// console.log(Object.values(user1))//[ 'Dimych', 'Valera', 'brown' ]
+
+
+
+const   numbers =[1,3,5,1,7,0,-2]
+let a = numbers[0]
+for (let i = 0; i<numbers.length; i++){
+    if(a < numbers[i+1]){
+        a= numbers[i+1]
     }
 }
-//console.log(user.address.city.title)
-//console.log(user["address"]["city"]["title"])
-let user1 ={
-    '0': 'Dimych',
-    '1': 'Valera'
-}
-// console.log(user1['0'])
- user1 ['table']= "brown"
-// console.log(user1)
- console.log(user1["table"]) //brown
-
-console.log(Object.keys(user1))//[ '0', '1', 'table' ]
-
-console.log(Object.values(user1))//[ 'Dimych', 'Valera', 'brown' ]
+console.log(a)
