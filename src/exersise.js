348e1277c8c2220ebd930b0d34587aa0d571492e
SergeyStarchenko <pdxserg@gmail.com>
2024-08-26 22:03:58 -0700

    Js

--- src/exersise.js
+++ src/exersise.js
@@ -24,11 +24,21 @@
 
 
 
-const   numbers =[1,3,5,1,7,0,-2]
-let a = numbers[0]
-for (let i = 0; i<numbers.length; i++){
-    if(a < numbers[i+1]){
-        a= numbers[i+1]
-    }
+// const   numbers =[1,3,5,1,7,0,-2]
+// let a = numbers[0]
+// for (let i = 0; i<numbers.length; i++){
+//     if(a > numbers[i+1]){
+//         a= numbers[i+1]
+//     }
+// }
+// console.log(a)
+
+
+const   fruits =["apple", "ddd", "apple", "banan", "orange" , "banan"]
+let appleCount =0
+for (let i = 0; i<fruits.length; i++){
+    if (fruits[i]=== 'apple'){
+        appleCount++
+   }
 }
-console.log(a)
+console.log(appleCount)
