2f587e258ec585aa2664414fb8ab2b54215826d9
SergeyStarchenko <pdxserg@gmail.com>
2025-02-12 12:21:41 -0800

    Js top question Fibonacci

--- src/JS/topQuestInterv.js
+++ src/JS/topQuestInterv.js
@@ -32,3 +32,16 @@
 //   return (n!==1) ? n*factorial(n-1):1
 // }
 //     console.log( factorial(5) ); // 120
+
+// 🟩 Fibonacci numbers
+const fibonacci = (n)=>{
+    let a = 1
+    let b = 1
+    for(let i = 3; i<= n; i++){
+        let c = a+b
+        a=b
+        b=c
+    }
+    return b
+}
+console.log(fibonacci(7))
