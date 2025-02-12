8714d4f4e20b8936e1a06f02a12fd7af02618626
SergeyStarchenko <pdxserg@gmail.com>
2025-02-12 11:14:32 -0800

    Js top question factorial

--- src/JS/topQuestInterv.js
+++ src/JS/topQuestInterv.js
@@ -13,16 +13,22 @@
 // }
 // console.log(findMinNumber(arrNum))
 
-// 🟩 find max number
-const arrNum=[2,3,6,100,7,12,-3,5,-11,23,1]
-const findMaxNumber= (arr)=>{
-    let maxNum = arr[0]
-    for (let i =0; i<arr.length;i++ ){
-        if(maxNum < arr[i]){
-            maxNum= arr[i]
-        }
-    }
-    return maxNum
-}
+// // 🟩 find max number
+// const arrNum=[2,3,6,100,7,12,-3,5,-11,23,1]
+// const findMaxNumber= (arr)=>{
+//     let maxNum = arr[0]
+//     for (let i =0; i<arr.length;i++ ){
+//         if(maxNum < arr[i]){
+//             maxNum= arr[i]
+//         }
+//     }
+//     return maxNum
+// }
+//
+// console.log(findMaxNumber(arrNum))
 
-console.log(findMaxNumber(arrNum))
+// // 🟩 factorial
+// const factorial = (n) => {
+//   return (n!==1) ? n*factorial(n-1):1
+// }
+//     console.log( factorial(5) ); // 120
